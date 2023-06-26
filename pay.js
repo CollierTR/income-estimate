815c36b8006d396ed3d411eaf186e5818786fa53
CollierTR <80648892+CollierTR@users.noreply.github.com>
2023-06-25 20:39:58 -0500

    Update pay.js

--- pay.js
+++ pay.js
@@ -97,7 +97,7 @@ function generatePay() {
     console.log('Income = ' + income); 
     */
 
-    document.getElementById('result').innerText = '$' + income.toLocaleString("en-US").toFixed(2);
+    document.getElementById('result').innerText = '$' + income.toLocaleString("en-US");
     document.getElementById('salary-display').innerText = '$' + baseSalary.toFixed(2);
     document.getElementById('below-target').innerText = '$' + belowTargetIncentive.toFixed(2);
 
